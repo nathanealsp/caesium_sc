998ab64e0f83177fccadf23d3cbd39c48a9b209e
Nytheneals <nythes@gmail.com>
2018-08-25 17:57:00 -0400

    refactored some code

--- promises.js
+++ promises.js
@@ -1,6 +1,3 @@
 const url = 'http://api.citybik.es/v2/networks';
 const cities = fetch(url);
-cities
-  .then(response => response)
-  .then(res => res.json())
-  .then(res => console.table(res.networks.map(item => item.name)));
+cities.then(res => res.json()).then(res => console.log(res.networks.map(item => item.name).sort()));
