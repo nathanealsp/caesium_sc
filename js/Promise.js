3c77e27611e2158747ba537dcfa18b2229cb72ac
nytheneals <nythes@gmail.com>
2018-08-24 20:29:36 -0400

    More Promises

---
+++ js/Promise.js
@@ -0,0 +1,25 @@
+// window.onload = function() {
+//   const url = 'http://api.citybik.es/v2/networks';
+//   const url2 = 'http://api.citybik.es/v2/networks/hubway';
+
+//   // const allCities = fetch(url)
+//   //   .then(data => data.json())
+//   //   .then(data => console.log('AllCities', data));
+//   // const boston = fetch(url2)
+//   //   .then(data => data.json())
+//   //   .then(data => console.log('Boston', data));
+// };
+
+// const keepApromise = false;
+
+// const allCities = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (keepApromise) {
+//       resolve('they keep one');
+//     } else {
+//       reject('they lied');
+//     }
+//   }, 5000);
+// });
+
+// console.log(allCities.then(res => console.log(res)).catch(err => Error(err)));
